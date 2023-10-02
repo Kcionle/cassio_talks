d8b677147f5849e93793e6134bd269e774de9d45
Cassio Pinheiro <124536816+Kcionle@users.noreply.github.com>
2023-10-02 18:32:59 -0300

    first commit

---
+++ dist/index.5967e6f2.js
@@ -0,0 +1,3 @@
+AOS.init();const e=new Date("Jun 05, 2024 19:00:00"),t=e.getTime(),n=setInterval(function(){let e=new Date,o=e.getTime(),a=t-o;document.getElementById("contador").innerHTML=`${Math.floor(a/864e5)}d ${Math.floor(a%864e5/36e5)}h ${Math.floor(a%36e5/6e4)}m ${Math.floor(a%6e4/1e3)}s `,a<0&&(clearInterval(n),document.getElementById("contador").innerHTML="Voc\xea chegou tarde")},1e3);//# sourceMappingURL=index.5967e6f2.js.map
+
+//# sourceMappingURL=index.5967e6f2.js.map
